48e4e86ba0b1cc03a527309c72837bcd804e147c
Lucas Caetano <lucas.matheu.caetano@gmail.com>
2022-05-14 23:23:07 -0300

    feat: add create deliveryman route, controller and use case

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,18 @@
 import { Router } from "express";
 import { AutenticateClientController } from "./modules/account/autenticateUser/autenticateClientController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientContoller";
+import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 
 const routes = Router();
 
 const createClientContoller = new CreateClientController();
 const autenticateClientController = new AutenticateClientController();
+const createDeliverymanController = new CreateDeliverymanController();
 
 routes.post("/authenticate/", autenticateClientController.handle);
 
 routes.post("/client/", createClientContoller.handle);
+
+routes.post("/deliveryman/", createDeliverymanController.handle);
 
 export { routes };
