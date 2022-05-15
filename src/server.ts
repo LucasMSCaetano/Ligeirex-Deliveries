92e504f5475a48215f7fea351489bfcec8778c5e
Lucas Caetano <lucas.matheu.caetano@gmail.com>
2022-05-14 21:46:00 -0300

    feat: add express config and dev script

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express, { request, response } from "express"
+
+const app = express()
+
+app.get("/", (request, response) =>{
+    return response.json({
+        message: "hello world"
+    })
+})
+
+app.listen(3000, () => console.log("server is running"))
