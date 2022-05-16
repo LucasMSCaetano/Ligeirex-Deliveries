21ae92779977c6d055ee69a5f4a4b2035bf298c7
Lucas Caetano <lucas.matheu.caetano@gmail.com>
2022-05-15 22:43:22 -0300

    feat: add delivery route, controller and use case. refactor on schema prisma for fix end_at field

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { AutenticateClientController } from "./modules/account/autenticateUser/autenticateClientController";
 import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/AuthenticateDeliverymanController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientContoller";
+import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 
 const routes = Router();
@@ -11,6 +12,7 @@ const autenticateClientController = new AutenticateClientController();
 const authenticateDeliverymanController = new AuthenticateDeliverymanController();
 const createClientContoller = new CreateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
+const createDeliveryController = new CreateDeliveryController();
 
 routes.post("/clients/authenticate", autenticateClientController.handle);
 routes.post("/deliveryman/authenticate", authenticateDeliverymanController.handle);
@@ -18,5 +20,7 @@ routes.post("/deliveryman/authenticate", authenticateDeliverymanController.handl
 routes.post("/client/", createClientContoller.handle);
 
 routes.post("/deliveryman/", createDeliverymanController.handle);
+
+routes.post("/delivery", createDeliveryController.handle)
 
 export { routes };
