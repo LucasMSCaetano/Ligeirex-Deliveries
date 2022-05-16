7cba55ce36c43dde82ae3fd3ca3b351bb3f8552e
Lucas Caetano <lucas.matheu.caetano@gmail.com>
2022-05-16 12:05:49 -0300

    feat: add flux for deliveries, to be accept and update by deliveryman

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { ensureAuthenticateClient } from "./middlewares/ensureAuthenticateClient";
+import { ensureAuthenticateDeliveryman } from "./middlewares/ensureAuthenticateDeliveryman";
 import { AutenticateClientController } from "./modules/account/autenticateUser/autenticateClientController";
 import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/AuthenticateDeliverymanController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientContoller";
 import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController";
+import { FindAllAvailableController } from "./modules/deliveries/useCases/findAllAvailable/FindAllAvailableController";
+import { UpdateDeliverymanController } from "./modules/deliveries/useCases/updateDeliveryman/UpdateDeliverymanController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 
 const routes = Router();
@@ -14,6 +17,8 @@ const authenticateDeliverymanController = new AuthenticateDeliverymanController(
 const createClientContoller = new CreateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
 const createDeliveryController = new CreateDeliveryController();
+const updateDeliverymanController = new UpdateDeliverymanController();
+const findAllAvailableController = new FindAllAvailableController();
 
 routes.post("/clients/authenticate", autenticateClientController.handle);
 routes.post("/deliveryman/authenticate", authenticateDeliverymanController.handle);
@@ -23,5 +28,9 @@ routes.post("/client/", createClientContoller.handle);
 routes.post("/deliveryman/", createDeliverymanController.handle);
 
 routes.post("/delivery", ensureAuthenticateClient, createDeliveryController.handle)
+
+routes.get("/delivery/available", ensureAuthenticateDeliveryman, findAllAvailableController.handle);
+
+routes.put("/delivery/updateDeliveryman/:id", ensureAuthenticateDeliveryman, updateDeliverymanController.handle)
 
 export { routes };
