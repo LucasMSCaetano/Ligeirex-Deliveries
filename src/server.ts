06c1eb6502fe212401a3152bf38e7d828df2b193
Lucas Caetano <lucas.matheu.caetano@gmail.com>
2022-05-23 08:48:39 -0300

    feat: add heroku config

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import app from "./config/App"
 import Environments from "./config/Environments"
-const { PORT } = Environments;
+//const { PORT } = Environments;
+const PORT = process.env.PORT || 3000
 
-app.listen(3000, () => console.log(`Listening at http://[::]:${PORT}`))
+app.listen(PORT, () => console.log(`Listening at http://[::]:${PORT}`))
